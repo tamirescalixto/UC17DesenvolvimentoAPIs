7655bdcdb4c8ddbdd566c15340e963f344408969
Tamires Calixto <tamires.americo.calixto@gmail.com>
2023-01-04 19:30:51 -0300

    UC17DesenvolvimentoAPIs

---
+++ EncontroRemoto1/ChapterSenaiUC17/ChapterSenaiUC17/Controllers/LivroController.cs
@@ -0,0 +1,32 @@
+﻿using ChapterSenaiUC17.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterSenaiUC17.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+
+        }
+
+        [HttpGet]
+        public IActionResult ler()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ EncontroRemoto1/ChapterSenaiUC17/ChapterSenaiUC17/Repositories/LivroRepository.cs
@@ -0,0 +1,20 @@
+﻿using ChapterSenaiUC17.Contexts;
+using ChapterSenaiUC17.Models;
+
+namespace ChapterSenaiUC17.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly Sqlcontext _context;
+        public LivroRepository(Sqlcontext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
